ac85d3195fd97dabf9fb23c9fafffc118c71f29a
Gopal Dahale <dahalegopal27@gmail.com>
2023-07-18 19:38:08 +0530

    Removed template code

--- setup.py
+++
@@ -1,7 +0,0 @@
-# See pyproject.toml for project configuration.
-# This file exists for compatibility with legacy tools:
-# https://setuptools.pypa.io/en/latest/userguide/pyproject_config.html
-
-from setuptools import setup
-
-setup()
