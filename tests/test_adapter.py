36f51703798a2e3968b5701fe26fec8dd9170699
Gopal Dahale <dahalegopal27@gmail.com>
2023-08-01 15:35:31 +0530

    basic tests

---
+++ tests/test_adapter.py
@@ -0,0 +1,192 @@
+from unittest import TestCase
+import pytest
+from qiskit_qulacs.adapter import convert_qiskit_to_qulacs_circuit
+from qiskit_qulacs.qulacs_backend import QulacsBackend
+from qulacs import QuantumState
+import numpy as np
+from qiskit import QuantumCircuit, BasicAer, execute
+from qiskit import extensions as ex
+from qiskit.circuit import Parameter
+from qiskit.circuit.library import PauliEvolutionGate
+from qiskit.quantum_info import SparsePauliOp
+from qiskit_qulacs.adapter import (
+    QISKIT_OPERATION_MAP,
+    QISKIT_GATE_TO_QULACS_GATE_MAPPING,
+)
+
+_EPS = 1e-10  # global variable used to chop very small numbers to zero
+
+qiskit_standard_gates = [
+    ex.IGate(),
+    ex.SXGate(),
+    ex.XGate(),
+    ex.CXGate(),
+    ex.RZGate(Parameter("λ")),
+    ex.RGate(Parameter("ϴ"), Parameter("φ")),
+    ex.C3SXGate(),
+    ex.CCXGate(),
+    ex.DCXGate(),
+    ex.CHGate(),
+    ex.CPhaseGate(Parameter("ϴ")),
+    ex.CRXGate(Parameter("ϴ")),
+    ex.CRYGate(Parameter("ϴ")),
+    ex.CRZGate(Parameter("ϴ")),
+    ex.CSwapGate(),
+    ex.CSXGate(),
+    ex.CUGate(Parameter("ϴ"), Parameter("φ"), Parameter("λ"), Parameter("γ")),
+    ex.CU1Gate(Parameter("λ")),
+    ex.CU3Gate(Parameter("ϴ"), Parameter("φ"), Parameter("λ")),
+    ex.CYGate(),
+    ex.CZGate(),
+    ex.CCZGate(),
+    ex.HGate(),
+    ex.PhaseGate(Parameter("ϴ")),
+    ex.RCCXGate(),
+    ex.RC3XGate(),
+    ex.RXGate(Parameter("ϴ")),
+    ex.RXXGate(Parameter("ϴ")),
+    ex.RYGate(Parameter("ϴ")),
+    ex.RYYGate(Parameter("ϴ")),
+    ex.RZZGate(Parameter("ϴ")),
+    ex.RZXGate(Parameter("ϴ")),
+    ex.XXMinusYYGate(Parameter("ϴ")),
+    ex.XXPlusYYGate(Parameter("ϴ")),
+    ex.ECRGate(),
+    ex.SGate(),
+    ex.SdgGate(),
+    ex.CSGate(),
+    ex.CSdgGate(),
+    ex.SwapGate(),
+    ex.iSwapGate(),
+    ex.SXdgGate(),
+    ex.TGate(),
+    ex.TdgGate(),
+    ex.UGate(Parameter("ϴ"), Parameter("φ"), Parameter("λ")),
+    ex.U1Gate(Parameter("λ")),
+    ex.U2Gate(Parameter("φ"), Parameter("λ")),
+    ex.U3Gate(Parameter("ϴ"), Parameter("φ"), Parameter("λ")),
+    ex.YGate(),
+    ex.ZGate(),
+]
+
+
+class TestAdapter(TestCase):
+    """Tests for the Adapter class."""
+
+    def test_state_preparation_01(self):
+        """Tests state_preparation handling of Adapter"""
+
+        input_state_vector = np.array([np.sqrt(3) / 2, np.sqrt(2) * complex(1, 1) / 4])
+
+        qiskit_circuit = QuantumCircuit(1)
+        qiskit_circuit.prepare_state(input_state_vector, 0)
+
+        qulacs_circuit_builder, _ = convert_qiskit_to_qulacs_circuit(
+            qiskit_circuit.decompose()
+        )
+        qulacs_circuit = qulacs_circuit_builder()
+        quantum_state = QuantumState(1)
+        qulacs_circuit.update_quantum_state(quantum_state)
+        output_state_vector = quantum_state.get_vector()
+
+        self.assertTrue(
+            (np.linalg.norm(input_state_vector - output_state_vector)) < _EPS
+        )
+
+    def test_state_preparation_00(self):
+        """Tests state_preparation handling of Adapter"""
+        input_state_vector = np.array([1 / np.sqrt(2), -1 / np.sqrt(2)])
+
+        qiskit_circuit = QuantumCircuit(1)
+        qiskit_circuit.prepare_state(input_state_vector, 0)
+
+        qulacs_circuit_builder, _ = convert_qiskit_to_qulacs_circuit(
+            qiskit_circuit.decompose()
+        )
+        qulacs_circuit = qulacs_circuit_builder()
+        quantum_state = QuantumState(1)
+        qulacs_circuit.update_quantum_state(quantum_state)
+        output_state_vector = quantum_state.get_vector()
+
+        self.assertTrue(
+            (np.linalg.norm(input_state_vector - output_state_vector)) < _EPS
+        )
+
+    def test_u_gate(self):
+        """Tests adapter conversion of u gate"""
+        qiskit_circuit = QuantumCircuit(1)
+        aer_backend = BasicAer.get_backend("statevector_simulator")
+
+        qiskit_circuit.u(np.pi / 2, np.pi / 3, np.pi / 4, 0)
+        job = execute(qiskit_circuit, aer_backend)
+
+        qulacs_circuit_builder, _ = convert_qiskit_to_qulacs_circuit(qiskit_circuit)
+        qulacs_circuit = qulacs_circuit_builder()
+        quantum_state = QuantumState(1)
+        qulacs_circuit.update_quantum_state(quantum_state)
+        qulacs_output = quantum_state.get_vector()
+
+        qiskit_output = np.array(job.result().get_statevector())
+
+        self.assertTrue(np.linalg.norm(qulacs_output - qiskit_output) < _EPS)
+
+    def test_standard_gate_decomp(self):
+        """Tests adapter decomposition of all standard gates to forms that can be translated"""
+        aer_backend = BasicAer.get_backend("statevector_simulator")
+        qulacs_backend = QulacsBackend()
+
+        for standard_gate in qiskit_standard_gates:
+            qiskit_circuit = QuantumCircuit(standard_gate.num_qubits)
+            qiskit_circuit.append(standard_gate, range(standard_gate.num_qubits))
+
+            parameters = standard_gate.params
+            if parameters:
+                parameter_values = [
+                    (137 / 61) * np.pi / i for i in range(1, len(parameters) + 1)
+                ]
+                parameter_bindings = dict(zip(parameters, parameter_values))
+                qiskit_circuit = qiskit_circuit.bind_parameters(parameter_bindings)
+
+            if standard_gate.name not in ["cu", "ecr"]:
+                # parameters are not binding to cu. I am not sure why.
+                # ecr generates a global phase. Not sure how to handle it.
+
+                with self.subTest(f"Circuit with {standard_gate.name} gate."):
+
+                    print("#" * 100)
+                    print(qiskit_circuit)
+
+                    qulacs_job = qulacs_backend.run(qiskit_circuit)
+                    qulacs_result = qulacs_job.result().get_statevector()
+
+                    qiskit_job = execute(qiskit_circuit, aer_backend)
+                    qiskit_result = qiskit_job.result().get_statevector()
+
+                    print(standard_gate.name)
+                    print("Qulacs Result:", qulacs_result)
+                    print("Qiskit Result:", qiskit_result)
+                    print("-" * 100)
+
+                    self.assertTrue(
+                        np.linalg.norm(qulacs_result - qiskit_result) < _EPS
+                    )
+
+    def test_exponential_gate_decomp(self):
+        """Tests adapter translation of exponential gates"""
+        aer_backend = BasicAer.get_backend("statevector_simulator")
+        qulacs_backend = QulacsBackend()
+        qiskit_circuit = QuantumCircuit(2)
+
+        hamiltonian = SparsePauliOp(["ZZ", "XI"], [1.0, -0.1])
+        evo = PauliEvolutionGate(hamiltonian, time=2)
+
+        qiskit_circuit.append(evo, range(2))
+        print(qiskit_circuit)
+
+        qulacs_job = qulacs_backend.run(qiskit_circuit)
+        qulacs_result = qulacs_job.result().get_statevector()
+
+        qiskit_job = execute(qiskit_circuit, aer_backend)
+        qiskit_result = qiskit_job.result().get_statevector()
+
+        self.assertTrue(np.linalg.norm(qulacs_result - qiskit_result) < _EPS)
