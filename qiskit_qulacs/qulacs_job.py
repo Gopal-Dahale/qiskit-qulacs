30b868357817a21aa3d3ec8e253b806e59116847
Gopal Dahale <dahalegopal27@gmail.com>
2023-11-28 14:15:41 +0530

    Using threadpool executor for running jobs

--- qiskit_qulacs/qulacs_job.py
+++ qiskit_qulacs/qulacs_job.py
@@ -1,59 +1,85 @@
 """Qulacs Job"""
-from typing import Optional
 
-from qiskit.providers import JobStatus, JobV1
-from qiskit.result import Result
-from qiskit.result.models import ExperimentResult, ExperimentResultData
+from qiskit.providers import JobError, JobStatus
+from qiskit.providers import JobV1 as Job
 
-
-def _get_result(tasks):
-    expt_results = []
-    for qulacs_state, circuit in tasks:
-        circuit.update_quantum_state(qulacs_state)
-        final_state = qulacs_state.get_vector()
-        data = ExperimentResultData(
-            statevector=final_state,
-        )
-        expt_results.append(
-            ExperimentResult(
-                shots=0,
-                success=True,
-                status=JobStatus.DONE,
-                data=data,
-            )
-        )
-
-    return expt_results
+from .backend_utils import DEFAULT_EXECUTOR, requires_submit
 
 
-class QulacsJob(JobV1):
+class QulacsJob(Job):
     """Qulacs Job"""
 
-    def __init__(self, task_id, tasks, backend, **metadata: Optional[dict]) -> None:
-        super().__init__(backend=backend, job_id=task_id, metadata=metadata)
-        self._task_id = task_id
-        self._backend = backend
-        self._metadata = metadata
-        self._tasks = tasks
+    def __init__(
+        self,
+        backend,
+        job_id,
+        fn,
+        circuits,
+        states,
+        run_options=None,
+        executor=None,
+    ) -> None:
+        super().__init__(backend, job_id)
+        self._fn = fn
+        self._circuits = circuits
+        self._states = states
+        self._run_options = run_options
+        self._executor = executor or DEFAULT_EXECUTOR
+        self._future = None
 
     def submit(self):
-        return
+        """Submit the job to the backend for execution."""
+        if self._future is not None:
+            raise JobError("Qulacs job has already been submitted.")
 
-    def task_id(self) -> str:
-        """Return a unique id identifying the task."""
-        return self._task_id
-
-    def result(self) -> Result:
-        experiment_results = _get_result(tasks=self._tasks)
-        return Result(
-            backend_name=self._backend,
-            backend_version=self._backend.version,
-            job_id=self._task_id,
-            qobj_id=0,
-            success=True,
-            results=experiment_results,
-            status=self.status(),
+        self._future = self._executor.submit(
+            self._fn,
+            self._circuits,
+            self._states,
+            self._run_options,
+            self._job_id,
         )
 
+    @requires_submit
+    def result(self, timeout=None):
+        """Get job result. The behavior is the same as the underlying
+        concurrent Future objects,
+        """
+        return self._future.result(timeout=timeout)
+
+    @requires_submit
+    def cancel(self):
+        """Attempt to cancel the job."""
+        return self._future.cancel()
+
+    @requires_submit
     def status(self):
-        return JobStatus.DONE
+        """Gets the status of the job by querying the Python's future"""
+        # The order is important here
+        if self._future.running():
+            _status = JobStatus.RUNNING
+        elif self._future.cancelled():
+            _status = JobStatus.CANCELLED
+        elif self._future.done():
+            _status = (
+                JobStatus.DONE if self._future.exception() is None else JobStatus.ERROR
+            )
+        else:
+            _status = JobStatus.INITIALIZING
+        return _status
+
+    def backend(self):
+        """Return the instance of the backend used for this job."""
+        return self._backend
+
+    def circuits(self):
+        """Return the list of QuantumCircuit submitted for this job.
+
+        Returns:
+            list of QuantumCircuit: the list of QuantumCircuit submitted for this job.
+        """
+        return self._circuits
+
+    def executor(self):
+        """Return the executor for this job"""
+        return self._executor
